6dc659b79348e6d94256ea4029192e45d1349fbb
drxcofn2023 <drxcofn2023@gmail.com>
2025-01-17 19:13:26 +0000

    Commit

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from './database.types';
 
-const supabaseUrl = 'https://ahjccbreeefhnnbtmhuz.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFoamNjYnJlZWVmaG5uYnRtaHV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzcxMjgyNDYsImV4cCI6MjA1MjcwNDI0Nn0.btkawMrfEBHxshOBVFMFuaROHv7qmH4KdAAsA4Ncjuo';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey);
